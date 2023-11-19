ff15f71eb3367d1bbd9cad16293fa4cd480da535
sakshishethr <sakshisheth0720@GMAIL.COM>
2023-11-19 18:24:44 -0500

    Created a Constants class housing the API key and base URL for OpenWeatherMap, centralizing configuration settings for easy management and modification.

--- Constants.cs
+++ Constants.cs
@@ -1,8 +1,15 @@
 ﻿namespace WeatherApp1
 {
+    // Class to hold constant values used throughout the application.
+    // This centralizes the configuration settings, making them easier to manage.
     internal class Constants
     {
+        // API key for OpenWeatherMap API.
+        // Important: Keep this key secure and do not expose it in public repositories.
         public const string API_KEY = "e0d1f860126c143dab0ab18ae2087118\n";
+
+        // Base URL for the OpenWeatherMap API.
+        // This is used to construct the full endpoint URLs in API requests.
         public const string API_BASE_URL = "https://api.openweathermap.org/";
     }
 }
