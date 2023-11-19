2770d903c282bbb660cb9d6ba0133a397e12545f
Pruthvi <pruthvisoni0505@gmail.com>
2023-11-19 18:00:13 -0500

    Made changes in the api model and added comments and explainations

--- Models/ApiModel/WeatherApiResponse.cs
+++ Models/ApiModel/WeatherApiResponse.cs
@@ -2,128 +2,167 @@
 
 namespace WeatherApp1.Models.ApiModel
 {
-    //API used : https://openweathermap.org/current#name
+    // Root class representing the entire JSON response from OpenWeatherMap API.
     public class WeatherApiResponseRoot
     {
+        // Coordinate object containing longitude and latitude.
         [JsonPropertyName("coord")]
         public WeatherApiResponseCoord Coord { get; set; }
 
+        // Array of weather conditions (id, main, description, icon).
         [JsonPropertyName("weather")]
         public WeatherApiResponseWeather[] Weather { get; set; }
 
+        // Internal parameter from the API (not typically used).
         [JsonPropertyName("_base")]
         public string _Base { get; set; }
 
+        // Main weather information like temperature, pressure, humidity, etc.
         [JsonPropertyName("main")]
         public WeatherApiResponseMain Main { get; set; }
 
+        // Visibility, measured in meters.
         [JsonPropertyName("visibility")]
         public int Visibility { get; set; }
 
+        // Wind information including speed and direction.
         [JsonPropertyName("wind")]
         public WeatherApiResponseWind Wind { get; set; }
 
+        // Cloudiness information.
         [JsonPropertyName("clouds")]
         public WeatherApiResponseClouds Clouds { get; set; }
 
+        // Time of data calculation, unix, UTC.
         [JsonPropertyName("dt")]
         public int Dt { get; set; }
 
+        // System data like country, sunrise and sunset times.
         [JsonPropertyName("sys")]
         public WeatherApiResponseSys Sys { get; set; }
 
+        // Shift in seconds from UTC.
         [JsonPropertyName("timezone")]
         public int Timezone { get; set; }
 
+        // City ID.
         [JsonPropertyName("id")]
         public int Id { get; set; }
 
+        // City name.
         [JsonPropertyName("name")]
         public string Name { get; set; }
 
+        // API status code.
         [JsonPropertyName("cod")]
         public int Cod { get; set; }
     }
 
+    // Represents geographic coordinates.
     public class WeatherApiResponseCoord
     {
+        // Longitude.
         [JsonPropertyName("lon")]
         public float Lon { get; set; }
 
+        // Latitude.
         [JsonPropertyName("lat")]
         public float Lat { get; set; }
     }
 
+    // Main weather data like temperature, pressure, humidity.
     public class WeatherApiResponseMain
     {
+        // Current temperature.
         [JsonPropertyName("temp")]
         public float Temp { get; set; }
 
+        // Human perception of weather.
         [JsonPropertyName("feels_like")]
         public float Feels_like { get; set; }
 
+        // Minimum temperature at the moment.
         [JsonPropertyName("temp_min")]
         public float Temp_min { get; set; }
 
+        // Maximum temperature at the moment.
         [JsonPropertyName("temp_max")]
         public float Temp_max { get; set; }
 
+        // Atmospheric pressure.
         [JsonPropertyName("pressure")]
         public int Pressure { get; set; }
 
+        // Humidity percentage.
         [JsonPropertyName("humidity")]
         public int Humidity { get; set; }
 
+        // Sea level pressure.
         [JsonPropertyName("sea_level")]
         public int Sea_level { get; set; }
 
+        // Ground level pressure.
         [JsonPropertyName("grnd_level")]
         public int Grnd_level { get; set; }
     }
 
+    // Wind information including speed and direction.
     public class WeatherApiResponseWind
     {
+        // Wind speed.
         [JsonPropertyName("speed")]
         public float Speed { get; set; }
 
+        // Wind direction, degrees.
         [JsonPropertyName("deg")]
         public int Deg { get; set; }
 
+        // Wind gust speed.
         [JsonPropertyName("gust")]
         public float Gust { get; set; }
     }
 
+    // Cloudiness information.
     public class WeatherApiResponseClouds
     {
+        // Cloudiness percentage.
         [JsonPropertyName("all")]
         public int All { get; set; }
     }
 
+    // System data like country, sunrise and sunset time.
     public class WeatherApiResponseSys
     {
+        // Country code.
         [JsonPropertyName("country")]
         public string Country { get; set; }
 
+        // Sunrise time, unix, UTC.
         [JsonPropertyName("sunrise")]
         public int Sunrise { get; set; }
 
+        // Sunset time, unix, UTC.
         [JsonPropertyName("sunset")]
         public int Sunset { get; set; }
     }
 
+    // Weather condition information.
     public class WeatherApiResponseWeather
     {
+        // Weather condition id.
         [JsonPropertyName("id")]
         public int Id { get; set; }
 
+        // Group of weather parameters (Rain, Snow, Extreme, etc.).
         [JsonPropertyName("main")]
         public string Main { get; set; }
 
+        // Weather condition within the group.
         [JsonPropertyName("description")]
         public string Description { get; set; }
 
+        // Weather icon id.
         [JsonPropertyName("icon")]
         public string Icon { get; set; }
     }
-
 }
--- Models/ApiModel/WeatherApiService.cs
+++ Models/ApiModel/WeatherApiService.cs
@@ -2,20 +2,27 @@
 
 namespace WeatherApp1.Models.ApiModel
 {
+    // Class responsible for handling API requests to fetch weather data.
     internal class WeatherApiService
     {
+        // HttpClient instance used to make HTTP requests.
         private readonly HttpClient _httpClient;
+
+        // Constructor initializes the HttpClient with the base URL of the API.
         public WeatherApiService()
         {
             _httpClient = new HttpClient();
             _httpClient.BaseAddress = new Uri(Constants.API_BASE_URL);
         }
 
+        // Asynchronously retrieves weather information for a specified city.
         public async Task<WeatherApiResponseRoot> GetWeatherInfo(string city)
         {
+            // Check for internet connectivity before making a request.
             if (Connectivity.Current.NetworkAccess != NetworkAccess.Internet)
-                return null;
+                return null; // Returns null if there's no internet connection.
 
+            // Asynchronously gets weather data from the API and deserializes the JSON response into WeatherApiResponseRoot object.
             return await _httpClient.GetFromJsonAsync<WeatherApiResponseRoot>($"data/2.5/weather?q={city}&units=metric&appid={Constants.API_KEY}");
         }
     }
